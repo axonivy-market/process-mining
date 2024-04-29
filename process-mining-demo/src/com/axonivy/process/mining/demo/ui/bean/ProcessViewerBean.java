825edc5804201721e7665a9081b9e7ad3ca12dba
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-04-29 17:51:08 +0700

    update process

--- process-mining-demo/src/com/axonivy/process/mining/demo/ui/bean/ProcessViewerBean.java
+++ process-mining-demo/src/com/axonivy/process/mining/demo/ui/bean/ProcessViewerBean.java
@@ -1,23 +1,98 @@
 package com.axonivy.process.mining.demo.ui.bean;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import org.apache.commons.lang3.StringUtils;
+import org.primefaces.shaded.json.JSONObject;
+import org.primefaces.shaded.json.JSONTokener;
+
+import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean
 @ViewScoped
 public class ProcessViewerBean {
 	private String selectedProcessName;
 	private String selectedModuleName;
+	private HashMap<String, String> fileMap = new HashMap<>();
+	private static final String DIARGAM_URL_PATTERN = "%s/process-editor/?pmv=%s%s&pid=%s&theme=light";
 
-	public List<String> getModuleName() {
-		return List.of("onboarding", "workforce-admin", "ticketting", "knowledge-base", "extra");
+	@PostConstruct
+	private void init() {
+		String processPath = Ivy.var().get("process-mining-demo.modulePath");
+		File folder = new File(processPath);
+		for (File module : folder.listFiles()) {
+			File processesFolder = getListOfItemInFolder(module).stream()
+					.filter(file -> file.getName().equalsIgnoreCase("processes")).findAny().orElse(null);
+			getProcessFilesFromProcessesFolder(processesFolder, module.getName());
+		}
 	}
 
-	public List<String> getProcessesName() {
-		return List.of("Initiate Pre Hire onboarding", "Dev Mock Data from queue", "Position maintainance",
-				"Image-Change-Award", "Update Worker Job Data");
+	public void confirm() throws IOException {
+		String appHomeRef = Ivy.html().applicationHomeRef();
+		String processId = fileMap.get(selectedProcessName);
+		String diagramUrl = DIARGAM_URL_PATTERN.formatted(appHomeRef, selectedModuleName, "%241", processId);
+		Ivy.log().warn(diagramUrl);
+	}
+
+	private void getProcessFilesFromProcessesFolder(File file, String pmvName) {
+		if (isFolder(file)) {
+			for (File subFile : getListOfItemInFolder(file)) {
+				getProcessFilesFromProcessesFolder(subFile, pmvName);
+			}
+		} else if (file != null && file.getName().endsWith(".json")) {
+			processJsonFile(file, pmvName);
+		}
+	}
+
+	private boolean isFolder(File file) {
+		return Objects.nonNull(file) && file.isDirectory() && file.listFiles() != null;
+	}
+
+	private List<File> getListOfItemInFolder(File file) {
+		if (isFolder(file)) {
+			return Arrays.asList(file.listFiles());
+		}
+		return new ArrayList<>();
+	}
+
+	private void processJsonFile(File jsonFile, String pmvName) {
+		try (FileReader reader = new FileReader(jsonFile)) {
+			JSONTokener tokener = new JSONTokener(reader);
+			JSONObject jsonObject = new JSONObject(tokener);
+			String id = jsonObject.getString("id");
+			String fileName = jsonFile.getName();
+			fileMap.put(pmvName + " " + fileName, id);
+		} catch (Exception e) {
+			Ivy.log().error(e.getMessage());
+		}
+	}
+
+	public String[] getModuleName() {
+		String processPath = Ivy.var().get("process-mining-demo.modulePath");
+		File folder = new File(processPath);
+		return Optional.ofNullable(folder).map(File::list).orElse(new String[0]);
+	}
+
+	public Set<String> getProcessesName() {
+		if (StringUtils.isBlank(selectedModuleName)) {
+			return fileMap.keySet();
+		}
+		return fileMap.keySet().stream().filter(name -> name.startsWith(selectedModuleName))
+				.collect(Collectors.toSet());
 	}
 
 	public String getSelectedModuleName() {
@@ -35,4 +110,5 @@ public class ProcessViewerBean {
 	public void setSelectedProcessName(String selectedProcessName) {
 		this.selectedProcessName = selectedProcessName;
 	}
+
 }
